f8f616ddec117fd8423171ed3f885ac690e914b9
JoaoZach <166412746+JoaoZach@users.noreply.github.com>
2025-12-07 17:21:27 +0000

    Unity Game Added

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Animator animator;
+    public float wallJumpCooldown { get; set;}
+    private Vector2 movement;
+    private Vector2 screenBounds;
+    private float playerHalfWidth;
+    private float xPosLastFrame;
+    private void Start()
+    {
+        
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        playerHalfWidth = spriteRenderer.bounds.extents.x;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        //ClampMovement();
+        FlipCharacterX();
+
+        if (wallJumpCooldown > 0 )
+        {
+            wallJumpCooldown -= Time.deltaTime;
+        }
+    }
+
+    private void FlipCharacterX()
+    {
+        float input = Input.GetAxis("Horizontal");
+        if (input > 0 && (transform.position.x > xPosLastFrame))
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (input < 0 && (transform.position.x < xPosLastFrame))
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        xPosLastFrame = transform.position.x;
+    }
+
+    private void ClampMovement()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, -screenBounds.x + playerHalfWidth, screenBounds.x - playerHalfWidth);
+        Vector2 pos = transform.position;
+        pos.x = clampedX;
+        transform.position = pos;
+    }
+
+    private void HandleMovement()
+    {
+        if (wallJumpCooldown > 0f) return;
+
+        float input = Input.GetAxis("Horizontal");
+        movement.x = input * speed * Time.deltaTime;
+        transform.Translate(movement);
+        if (input != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+}
