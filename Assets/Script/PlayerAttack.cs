6952ef03775890d7cc679ba5998d6a29c32877b1
JoaoZach <166412746+JoaoZach@users.noreply.github.com>
2025-12-18 22:20:59 +0000

    fixed some stuff

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -2,45 +2,38 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public Animator animator;
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public int damage = 1;
     public LayerMask enemyLayer;
-    [Tooltip("Radius to notify nearby enemies that the player performed an attack (they won't flee)")]
-    public float notifyRadius = 1.5f;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            animator?.SetTrigger("Attack");
-   
-            var movementState = GetComponent<PlayerMovementState>();
-            if (movementState != null)
-                movementState.SetMoveState(PlayerMovementState.MoveState.Attack);
-
-            PerformAttack(); 
+            var state = GetComponent<PlayerMovementState>();
+            if (state != null)
+                state.SetMoveState(PlayerMovementState.MoveState.Attack);
         }
     }
 
     public void PerformAttack()
     {
         if (attackPoint == null) return;
-        var hits = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
-        foreach (var c in hits)
-        {
-            c.gameObject.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
-        }
 
-        var nearby = Physics2D.OverlapCircleAll(attackPoint.position, notifyRadius, enemyLayer);
-        foreach (var c in nearby)
+        Collider2D[] hits = Physics2D.OverlapCircleAll(
+            attackPoint.position,
+            attackRange,
+            enemyLayer
+        );
+
+        foreach (var hit in hits)
         {
-            var enemy = c.GetComponentInParent<EnemyBT>();
-            if (enemy != null)
-            {
-                enemy.OnPlayerAttackNearby();
-            }
+            hit.SendMessage(
+                "TakeDamage",
+                damage,
+                SendMessageOptions.DontRequireReceiver
+            );
         }
     }
 
